388955739683eb869d7b6a549a322a0d38cee423
overdose-dev <chinazauchenna@gmail.com>
2023-02-16 16:10:08 -0800

    types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)". sizeof(char));
+	printf("size of int: %lu bytes(s)". sizeof(int));
+	printf("size of a long int: %lu byte(s)". sizeof(long int));
+	printf("size of a long long int: %lu bytes(s)". sizeof(long long int));
+	printf("Size of a float: %lu byte(s)". sizeof(float));
+	return (0);
+}
