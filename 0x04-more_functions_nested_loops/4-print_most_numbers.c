6eb1ab787c02d2f292b278dcab1eb5cfe0ec50a6
overdose-dev <chinazauchenna@gmail.com>
2023-02-24 12:37:11 -0800

    prints numbers but 2 & 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints numbers
+ *
+ * Return: always 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	if (i != 2 && i != 4)
+	{
+	for (i = 0 ; i <= 10 ; i++)
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
