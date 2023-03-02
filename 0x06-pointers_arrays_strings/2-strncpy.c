bff2c4a043924c5d28af22d77ee8fa7e892b0729
overdose-dev <chinazauchenna@gmail.com>
2023-03-02 01:13:13 -0800

    function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strncpy - funtion that copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: the number of strings
+ * Return: destination string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+		dest[i] = src[i];
+	return (dest);
+}
