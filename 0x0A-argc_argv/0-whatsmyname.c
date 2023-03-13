ba9eb00ddc522eda57cd04fa5d0d481a272797d3
overdose-dev <chinazauchenna@gmail.com>
2023-03-13 06:08:28 -0700

    function that prints its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints its name
+ * @argc: argument count
+ * @argv: array of commands listed
+ * Return: 0 always (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is the readme file for Project 0x0A. C - argc, argv
