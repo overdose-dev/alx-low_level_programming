c919fd58a7e3834ff4053f7df8e1271d94f1efc7
overdose-dev <chinazauchenna@gmail.com>
2023-03-14 04:35:02 -0700

    function that concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - length of string
+ * @s: array of elements
+ * Return: 1
+ */
+
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (1);
+}
+
+/**
  * str_concat - concatenates two strings
  * @s1: input parameter
  * @s2: input parameter
@@ -10,34 +28,29 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, j;
+	char *mlc
+	unsigned int i, j, size;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[i] != '\0')
-		i++;
-	while (s2[j] != '\0')
-		j++;
-	conct = malloc(sizeof(char) * (i + j + 1));
+	size = (_strlen(s1) + _strlen(s2) + 1);
 
-	if (conct == NULL)
+	mlc = (char *) malloc(size * sizeof(char));
+
+	if (mlc == 0)
+	{
 		return (NULL);
+	}
+	for (i = 0 ; *(s1 + i) != '\0' ; i++)
+		*(mlc + i) = *(s1 + i);
 
-	i = j = 0;
-	while (s1[i] != '\0')
+	for (j = 0 ; *(s2 + j) != '\0' ; j++)
 	{
-		conct[i] = s1[i];
+		*(dst + i) = *(s2 + j);
 		i++;
 	}
-	while (s2[j] != '\0')
-	{
-		conct[i] = s2[j];
-		i++, j++;
-	}
-	conct[i] = '\0';
-	return (conct);
+	return (mlc);
 }
